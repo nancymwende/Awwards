c9e059552e973c4e2ede0aaecde8db871f9427c6
bkee9 <nancy.mwende@student.moringaschool.com>
2022-06-12 18:31:13 +0300

    ft:create the editprofile function

--- Awardapp/views.py
+++ Awardapp/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
+from Awardapp.forms import NewUserform,UserProfile,EditProfile
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-def heading(request):
-    return HttpResponse('wELCOME TO THE aWARDs')
+def welcome(request):
+    return render(request,'index.html')
+
+def register(request):
+    form = NewUserform()
+    
+    if request.method == 'POST':
+        form = NewUserform(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, 'register.html',{'form':form})
+    
+def login(request):
+    return render(request, 'login.html')    
+    
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfile(request.POST,request.FILES,instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserProfile()
+    return render(request,'profile.html', {'form':form})
+    
+def editprofile(request):
+    if request.method == 'POST':
+        form = EditProfile(request.POST,request.FILES,instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = EditProfile()
+    return render(request,'profile.html', {'form':form})    
