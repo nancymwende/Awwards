c5c573bf3f464cf6eadd3bc1c753b079c595c8b3
bkee9 <nancy.mwende@student.moringaschool.com>
2022-06-14 08:42:14 +0300

    ft:register profile $ post urls

--- Awardapp/urls.py
+++ Awardapp/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('',views.welcome,name='welcome')
+    path('',views.welcome,name='welcome'),
+    path('register', views.register, name = 'register'),
+    path('login',views.login,name = 'login'),
+    path('profile',views.profile,name= 'profile'),
+    path('editprofile',views.editprofile,name='editprofilre'),
+    path('api/profiles',views.ProfileList.as_view()),
+    path('api/posts',views.PostList.as_view())
 ]
