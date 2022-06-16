eb89666f3c161e56582bd32a38d6f3901b6a4c72
bkee9 <nancy.mwende@student.moringaschool.com>
2022-06-16 22:10:07 +0300

    ft:add delele profile function

--- Awardapp/models.py
+++ Awardapp/models.py
@@ -9,30 +9,36 @@ class Profile(models.Model):
         bio = models.TextField(max_length=100,blank=True)
         contact = models.EmailField(blank=True, max_length=100)
 
-        def __str__(self):
-                return self.contact
+        def save_profile(self):
+                self.save()
+                
+        def create_profile(self):
+                self.save()        
 
         def update(self):
                 self.save()    
         
         def delete_profile(self):
                 self.delete()
-@classmethod
-def get_profile_by_user(cls, searched_term):
-        profile = cls.objects.filter(name__icontains=searched_term)
-        return profile 
+        @classmethod
+        def get_profile_by_user(cls, searched_term):
+                profile = cls.objects.filter(name__icontains=searched_term)
+                return profile 
+        
+        def __str__(self):
+                return self.contact
         
         
 class Post(models.Model):
-        user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='post',)
+        user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post',)
         image = CloudinaryField('profile_photo')
         url = models.URLField(max_length=100,blank=True)
         description= models.TextField(max_length=100,blank=True)
         contact = models.EmailField(blank=True, max_length=100)
         title = models.CharField(blank=True,max_length=100)
         
-        def __str__(self):
-                return self.contact
+        def save_post(self):
+                self.save()
 
         def update(self):
                 self.save()    
@@ -44,6 +50,7 @@ def get_post_by_user(cls, searched_term):
         post = cls.objects.filter(name__icontains=searched_term)
         return post 
         
+        
 class Rating(models.Model):
         user = models.ForeignKey(User,on_delete=models.CASCADE)
         post = models.ForeignKey(Post,on_delete=models.CASCADE)
@@ -51,6 +58,12 @@ class Rating(models.Model):
         usability_rate = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)],default=0)
         content_rate = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)],default=0)
         average_score = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)],default=0)
+        
+        def save_rating(self):
+                self.save()
 
+        def delete_rating(self):
+                self.delete()
+        
         def __str__(self):
                 return f'{self.post}'        
